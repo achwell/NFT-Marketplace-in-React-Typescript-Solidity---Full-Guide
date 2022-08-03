6c260180e9a1440eb190850c995a9cb2a5b9fcf2
Axel Wulff Sæther <axel.sather@soprasteria.com>
2022-08-03 23:25:36 +0200

    Etter Section 5: Web3 Provider

---
+++ nft-marketplace/components/providers/web3/index.tsx
@@ -0,0 +1,37 @@
+import {createContext, FunctionComponent, PropsWithChildren, useContext, useEffect, useState} from "react"
+import { ethers } from "ethers"
+import { createDefaultState, Web3State } from "./utils"
+
+const Web3Context = createContext<Web3State>(createDefaultState())
+
+const Web3Provider: FunctionComponent<PropsWithChildren> = ({children}) => {
+
+    const [web3Api, setWeb3Api] = useState<Web3State>(createDefaultState())
+
+    useEffect(() => {
+        function initWeb3() {
+            const provider = window.ethereum ? new ethers.providers.Web3Provider(window.ethereum as any): null
+
+            setWeb3Api({
+                ethereum: window.ethereum,
+                provider,
+                contract: null,
+                isLoading: false
+            })
+        }
+
+        initWeb3()
+    }, [])
+
+    return (
+        <Web3Context.Provider value={web3Api}>
+            {children}
+        </Web3Context.Provider>
+    )
+}
+
+export function useWeb3() {
+    return useContext(Web3Context)
+}
+
+export default Web3Provider
