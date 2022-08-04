c4a2a34fd0949b7b7e76db2a89238f951a71cad6
Axel Wulff Sæther <axel.sather@soprasteria.com>
2022-08-04 09:40:43 +0200

    Etter Section 8: Account Hook

--- nft-marketplace/components/providers/web3/index.tsx
+++ nft-marketplace/components/providers/web3/index.tsx
@@ -1,7 +1,7 @@
 import {createContext, FC, ReactNode, useContext, useEffect, useState} from "react"
 import {ethers} from "ethers"
 import {createDefaultState, createWeb3State, loadContract, Web3State} from "./utils"
-import { setupHooks } from "@hooks/web3/setupHooks"
+import {setupHooks} from "@hooks/web3/setupHooks"
 
 interface Props {
     children: ReactNode
@@ -15,14 +15,22 @@ const Web3Provider: FC<Props> = ({children}) => {
 
     useEffect(() => {
         async function initWeb3() {
-            const provider = new ethers.providers.Web3Provider(window.ethereum as any)
-            const contract = await loadContract("NftMarket", provider)
-            setWeb3Api(createWeb3State({
-                ethereum: window.ethereum,
-                provider,
-                contract,
-                isLoading: false
-            }))
+            try {
+                const provider = new ethers.providers.Web3Provider(window.ethereum as any)
+                const contract = await loadContract("NftMarket", provider)
+                setWeb3Api(createWeb3State({
+                    ethereum: window.ethereum,
+                    provider,
+                    contract,
+                    isLoading: false
+                }))
+            } catch (e: any) {
+                console.error("Please, install web3 wallet");
+                setWeb3Api((api) => createWeb3State({
+                    ...api as any,
+                    isLoading: false,
+                }))
+            }
         }
 
         initWeb3()
@@ -40,8 +48,8 @@ export function useWeb3() {
 }
 
 export function useHooks() {
-    const { hooks } = useWeb3();
-    return hooks;
+    const {hooks} = useWeb3()
+    return hooks
 }
 
 export default Web3Provider
