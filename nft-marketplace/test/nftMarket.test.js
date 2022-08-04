c2e066b77225ee91d7f8e65155e1213338e92417
Axel Wulff Sæther <axel.sather@soprasteria.com>
2022-08-04 10:27:21 +0200

    Etter Section 10: Mint Token - Smart Contract

---
+++ nft-marketplace/test/nftMarket.test.js
@@ -0,0 +1,47 @@
+const NftMarket = artifacts.require("NftMarket");
+const { ethers } = require("ethers");
+
+contract("NftMarket", accounts => {
+    let _contract = null;
+
+    before(async () => {
+        _contract = await NftMarket.deployed();
+        let _nftPrice = ethers.utils.parseEther("0.3").toString();
+        let _listingPrice = ethers.utils.parseEther("0.025").toString();
+    })
+
+    describe("Mint token", () => {
+        const tokenURI = "https://test.com";
+        before(async () => {
+            await _contract.mintToken(tokenURI, _nftPrice,  {
+                from: accounts[0],
+                value: _listingPrice
+            })
+        })
+
+        it("owner of the first token should be address[0]", async () => {
+            const owner = await _contract.ownerOf(1)
+            assert.equal(owner, accounts[0], "Owner of token is not matching address[0]")
+        })
+
+        it("first token should point to the correct tokenURI", async () => {
+            const actualTokenURI = await _contract.tokenURI(1)
+            assert.equal(actualTokenURI, tokenURI, "tokenURI is not correctly set");
+        })
+
+        it("should not be possible to create a NFT with used tokenURI", async () => {
+            try {
+                await _contract.mintToken(tokenURI, _nftPrice, {
+                    from: accounts[0]
+                })
+            } catch(error) {
+                assert(error, "NFT was minted with previously used tokenURI");
+            }
+        })
+        it("should have one listed item", async () => {
+            const listedItemCount = await _contract.listedItemsCount();
+            assert.equal(listedItemCount.toNumber(), 1, "Listed items count is not 1");
+        })
+    })
+
+})
